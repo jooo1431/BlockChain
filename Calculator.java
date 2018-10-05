c9875a856547f99b87742603730f06d01aedf125
조윤영 <yoon980208@gmail.com>
2018-10-05 13:54:33 +0900

    마이너스 추가

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,9 @@
 public class Calculator {
     public static void main(String[] args) {
         print("hahaha");
-        
+
     }
+    public static int minus(int a, int b){return a-b;
+    }
+    
 }
